d8913c212028fa6f4330605023762a250ef13c3f
Lukas Araujo Santiago <lukas.santiago@integradora.com.br>
2020-07-27 14:07:54 -0300

    test failure

--- src/script.test.js
+++ src/script.test.js
@@ -3,6 +3,14 @@ import script from './script';
 describe('Bin2Dec', () => {
 	
 	it('isBinary', () => {
-		expect(script.bin2dec(10.01)).toBe(true);
+		expect(script.ValidateInput('101.01')).toBe(true);
+	});
+	
+	it('isBinary', () => {
+		expect(script.ValidateInput('101.0.1')).toBe(false);
+	});
+	
+	it('isBinary', () => {
+		expect(script.ValidateInput('101.01')).toBe(false);
 	});
 });
